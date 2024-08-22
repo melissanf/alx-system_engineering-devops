35fd8dc2fe3307f909c551a76107a0b47c80ef35
melissanf <138734144+melissanf@users.noreply.github.com>
2024-08-22 23:16:56 +0200

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,15 +1,5 @@
-# Increases the amount of traffic an Nginx server can handle by increasing the ULIMIT
-
+# Fix request limit at nginx
 exec { 'fix-for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/bin:/usr/bin',
+    command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
 }
-
-# Ensure that the file descriptor changes are applied before restarting Nginx
-Exec['fix-for-nginx'] -> Exec['nginx-restart']
-
-# Restart Nginx to apply the changes
-exec { 'nginx-restart':
-  command => '/usr/sbin/nginx -s reload', # Adjust this path if necessary
-  path    => '/bin:/usr/bin',
-}
+# Restart nginx
