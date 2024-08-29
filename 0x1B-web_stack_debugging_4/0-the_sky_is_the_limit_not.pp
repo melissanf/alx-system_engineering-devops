e53c6fca7c84bfbd3af92e1dee186b3562b19bf1
melissanf <138734144+melissanf@users.noreply.github.com>
2024-08-30 00:08:13 +0200

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,23 +1,13 @@
 # Increases the amount of traffic an Nginx server can handle.
 
 # Increase the ULIMIT of the default file
-exec { 'increase-ulimit':
+exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => ['/bin', '/usr/bin'], # Correct paths for sed
-  unless  => 'grep -q "4096" /etc/default/nginx', # Avoid unnecessary changes
-}
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
-# Ensure system-wide file descriptor limits are increased
-file { '/etc/security/limits.conf':
-  ensure  => file,
-  content => "nginx soft nofile 4096\nnginx hard nofile 4096\n",
-  mode    => '0644',
-}
-
-# Restart Nginx service to apply changes
+# Restart Nginx
 exec { 'nginx-restart':
-  command     => '/usr/sbin/nginx -s reload', # Use the reload command for a graceful restart
-  path        => ['/bin', '/usr/bin', '/usr/sbin'], # Ensure paths include /usr/sbin
-  refreshonly => true, # Only run when notified
-  notify      => Exec['increase-ulimit'], # Notify this exec to run after the ulimit change
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
